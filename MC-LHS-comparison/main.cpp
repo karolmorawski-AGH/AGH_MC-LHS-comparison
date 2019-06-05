02d14a33d8f69abe92659a49c43baef3bec7e665
Karol Morawski <karol.morawski4@gmail.com>
2019-06-05 23:04:32 +0200

    tesrt

--- MC-LHS-comparison/main.cpp
+++ MC-LHS-comparison/main.cpp
@@ -11,16 +11,16 @@
 typedef unsigned int uint;
 
 /*Defines area and its volume*/
-const float XMIN = -5.f;
-const float XMAX = 5.f;
-const float YMIN = -5.f;
-const float YMAX = 5.f;
+const float XMIN = -10.f;
+const float XMAX = 10.f;
+const float YMIN = -10.f;
+const float YMAX = 10.f;
 const float ZMAX = 10.f;
 
 /*Sets number of randomly generated points*/
-const uint PRECISION = 10000;
-const uint partitions = 10;
-const uint packages = 2;
+const uint PRECISION = 1000000;
+const uint partitions = 25;
+const uint packages = 60;
 
 int main()
 { 
@@ -29,7 +29,7 @@ int main()
 	/*Monte Carlo*/
 	std::cout << "MC: \t";
 	auto start = high_resolution_clock::now();
-	std::cout << monte_carlo_start(simplePlane, XMIN, XMAX, YMIN, YMAX, ZMAX, PRECISION);
+	std::cout << monte_carlo_start(matyas, XMIN, XMAX, YMIN, YMAX, ZMAX, PRECISION);
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<milliseconds>(stop - start);
 	std::cout << " \t\t" << duration.count() << "ms using " << PRECISION << " randomly generated points" << std::endl;
@@ -37,7 +37,7 @@ int main()
 	/*LHS*/
 	std::cout << "LHS: \t";
 	auto startlhs = high_resolution_clock::now();
-	std::cout << lhs_start(simplePlane, XMIN, XMAX, YMIN, YMAX, ZMAX, partitions, packages);
+	std::cout << lhs_start(matyas, XMIN, XMAX, YMIN, YMAX, ZMAX, partitions, packages);
 	auto stoplhs = high_resolution_clock::now();
 	auto durationlhs = duration_cast<milliseconds>(stoplhs - startlhs);
 	std::cout << " \t" << durationlhs.count() << "ms using " << partitions * partitions * partitions * packages << " randomly generated points" << std::endl;
